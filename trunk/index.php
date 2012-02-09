4c1286f56bd90bc91fba5bf4899e4e2ed4fd5096
mirelon@gmail.com <mirelon@gmail.com@eaa8846b-a0ef-c4df-bbf9-d4cd15ed3af3>
2012-02-09 21:36:23 +0000

    Updated jquery

--- trunk/index.php
+++ trunk/index.php
@@ -1,3 +1,23 @@
+<script type="text/javascript" src="js/jquery-1.7.1.js"></script>
+<script type="text/javascript" src="js/jquery.pause.js"></script>
+<script type="text/javascript" src="js/display.js"></script>
+<link type="text/css" rel="stylesheet" href="style.css" />
+<body>
+<?php require_once('menu.php'); ?>
+<textarea name="popis" rows="20">
 <?php
-header('Location: edit.php?drillname=diamond');
+if(isset($_GET['drillname']))
+{
+  $filename = $_GET['drillname'];
+  $filename = 'data/' . preg_replace("/[^a-z]+/", "", $filename) . '.txt';
+
+  if(file_exists($filename))
+  {
+    echo file_get_contents($filename);
+  }
+}
 ?>
+</textarea>
+<div id="playfield"></div>
+<div class="control-panel"><button id="pause-button">PLAY</button></div>
+</body>
